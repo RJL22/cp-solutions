db886af7ec107e6eb175312a484b05a3bc5acfb9
RJL22 <ryanjlee22@gmail.com>
2021-01-24 14:58:08 -0500

    Added dfs example #1

---
+++ dfs_example1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+/*
+This program finds all the possible letter combinations for a given phone number.
+Note: The numbers '0' and '1' are not associated with any letters
+*/
+
+//                           Index:  0   1     2      3      4      5      6      7       8      9
+std::vector<std::string> digitMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+void generateStrings(std::vector<std::string>& solutions, std::string& digits, std::vector<std::string>& digitMap, int index, std::string curr)
+{
+	if (curr.length() == digits.length())
+	{
+		solutions.push_back(curr);
+		return;
+	}
+
+	std::string possibilities = digitMap[digits[index] - '0'];
+
+	for (int i = 0; i < possibilities.length(); i++)
+		generateStrings(solutions, digits, digitMap, index + 1, curr + possibilities[i]);
+}
+
+int main()
+{
+	std::vector<std::string> solutions;
+
+	std::string number = "222";
+	std::string empty = "";
+
+	generateStrings(solutions, number, digitMap, 0, empty);
+
+	for (int i = 0; i < solutions.size(); i++)
+		std::cout << solutions[i] << std::endl;
+
+
+
+	return 0;
+}
